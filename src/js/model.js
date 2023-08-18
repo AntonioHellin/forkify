74463c00d836039bb616b9a9c55170ca0f2ed497
antoniohellin <antonio.hellinh@gmail.com>
2023-08-18 09:07:42 +0200

    Implementing bookmarks 2

--- src/js/model.js
+++ src/js/model.js
@@ -28,6 +28,13 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id)) {
+      state.recipe.bookmarked = true;
+    } else {
+      state.recipe.bookmarked = false;
+    }
+    console.log(state.recipe);
   } catch (err) {
     console.error(`${err} ERROR !!!`);
     throw err;
@@ -78,4 +85,13 @@ export const addBookmark = function (recipe) {
 
   // Mark current recipe as bookmarks
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+};
+
+export const deleteBookmark = function (id) {
+  // Remove bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as NOT bookmarks
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
 };
