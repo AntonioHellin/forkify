9d5aea489cbd35fc7c09fd311e173c90e94c58ed
antoniohellin <antonio.hellinh@gmail.com>
2023-08-23 23:43:19 +0200

    Uploading a new recipe. Render the new recipe in a different view

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
@@ -91,10 +92,28 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
-const controlAddRecipe = function (newRecipe) {
-  console.log(newRecipe);
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show spinner
+    addRecipeView.renderSpinner();
+    // Upload new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
 
-  // Upload new recipe data
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Display success message
+    addRecipeView.renderMessage();
+
+    // Close form window to add recipe
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error('ERROR', err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 const init = function () {
